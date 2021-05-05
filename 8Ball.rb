0e9d7096a3dc64f23084c7edbcc2086e33fc4217
Cole Wilson <cole@colewilson.xyz>
2021-05-04 19:46:49 -0700

    Initial Release

--- 8Ball.rb
+++ 8Ball.rb
@@ -1,14 +1,14 @@
 # Created with sailboat, the Python releaser
 
-# v0.0.3
+# v0.0.1
 
 class 8ball < Formula
   include Language::Python::Virtualenv
 
-  desc "8 ball for the terminal"
-  homepage "https://github.com"
-  url "https://files.pythonhosted.org/packages/2a/38/d904d4bdaf4163ce60c9c18276397a1942a93586c74e6251c988b7d44fdd/8ball_cole_wilson_pycon_demo-0.0.3.tar.gz" # These lines must be configured during release, not build.
-  sha256 "55599414884217d1a5b894308010454cabc5f3c23e50f883738a0da871b5cb77" # ^^^
+  desc "8ball for the terminal"
+  homepage "https://github.com/"
+  url "https://files.pythonhosted.org/packages/f2/49/1ebaea5238f8cf158867aacc451db20f7e327d9a4171c217a756e93a79b7/cw_pycon_8ball-0.0.1.tar.gz" # These lines must be configured during release, not build.
+  sha256 "eec64cfeb325ba438026a616ff92559ca1554d680b52f2293c11ea7b5ecd9588" # ^^^
   license "MIT"
 
   livecheck do
