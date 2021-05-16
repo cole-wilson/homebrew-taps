c435b16829a6dd836fb9e34a302db77bf113c75f
Cole Wilson <cole@colewilson.xyz>
2021-05-16 12:29:15 -0700

    Release 2

--- Test_Project_4.rb
+++ Test_Project_4.rb
@@ -1,14 +1,14 @@
 # Created with sailboat, the Python releaser
 
-# v0.0.1+773fd47.1
+# v0.0.2
 
 class Test_Project_4 < Formula
   include Language::Python::Virtualenv
 
   desc "short"
   homepage "https://google.com"
-  url "https://files.pythonhosted.org/packages/e9/d5/4b26b67b75ad9552110c3012368e865943335ab3f088bc39e4d36baa071d/pyconus_test4-0.0.1.tar.gz" # These lines must be configured during release, not build.
-  sha256 "4d115a80d24dec444fc2b484e8c01412db6a17383774c85add764353a14fd3ee" # ^^^
+  url "https://files.pythonhosted.org/packages/6f/55/ecc8dfb98d5ecfa406862ae80d70f82bfc490d32e80f17d3fb67de7b6742/pyconus_test4-0.0.2.tar.gz" # These lines must be configured during release, not build.
+  sha256 "bac0f21346104f39b8fa437688365d804f8f10244a27c61f42c0990ad1777c37" # ^^^
   license "none"
 
   livecheck do
