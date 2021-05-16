6e992879b25522431b9dd1056dafad1eca775994
COle Wilson <cole@colewilson.xyz>
2021-05-16 11:27:11 -0700

    release 2

--- Test_3.rb
+++ Test_3.rb
@@ -1,14 +1,14 @@
 # Created with sailboat, the Python releaser
 
-# v0.0.1
+# v0.0.2
 
 class Test_3 < Formula
   include Language::Python::Virtualenv
 
   desc "short"
   homepage "https://github.com"
-  url "https://files.pythonhosted.org/packages/22/39/d5cd9f17039f47912e82d2a05a7014d8869dca3dca32586407717b1fe989/test3_pycon-0.0.1.tar.gz" # These lines must be configured during release, not build.
-  sha256 "5a7db6c084bb39da37fcb68901842fc3a95242ca3f7dcd3d2d2161bddf354a10" # ^^^
+  url "https://files.pythonhosted.org/packages/35/fb/4f1677321c92174f6eeaddcf36b28fe105345e1f6b9b71d3d5cb821873b7/test3_pycon-0.0.2.tar.gz" # These lines must be configured during release, not build.
+  sha256 "712c1408ba358f71f17277f8b6721a7a7b408fa656b967014f61884f06a6002a" # ^^^
   license "none"
 
   livecheck do
